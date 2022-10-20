886c606789dab52dca71ea8ca54b82edd6d15e21
arjuuunhm <96384102+arjuuunhm@users.noreply.github.com>
2022-10-20 15:33:25 -0400

    Commit test

--- src/pigeonhole.ml
+++ src/pigeonhole.ml
@@ -4,5 +4,5 @@ type bird_specie = Pigeon | Cardinal | Owl | BlueJay | KingFisher
 (** The type of bird that contains the following fields: species, size, hits *)
 type bird = {species : bird_specie; size : int; hits : int}
 
-(***)
+(** Type to identify tiles on the game board*)
 type coordinate = char * int
