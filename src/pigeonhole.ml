4a098833a8c7963656a560d6c2d9ecff7be57dc5
abhigoyal29 <83799253+abhigoyal29@users.noreply.github.com>
2022-10-20 15:27:17 -0400

    Testing changes with github, added changes to pigeonhole file with types

---
+++ src/pigeonhole.ml
@@ -0,0 +1,8 @@
+(** The type of bird that occupy the board *)
+type bird_specie = Pigeon | Cardinal | Owl | BlueJay | KingFisher
+
+(** The type of bird that contains the following fields: species, size, hits *)
+type bird = {species : bird_specie; size : int; hits : int}
+
+(***)
+type coordinate = char * int
