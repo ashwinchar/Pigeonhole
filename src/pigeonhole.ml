399b1c97aa0134eebfa0d0a4d02cb4709ee8d6db
kirtidahal07 <109311172+kirtidahal07@users.noreply.github.com>
2022-10-20 15:33:08 -0400

    Demo

--- src/pigeonhole.ml
+++ src/pigeonhole.ml
@@ -1,5 +1,5 @@
 (** The type of bird that occupy the board *)
-type bird_specie = Pigeon | Cardinal | Owl | BlueJay | KingFisher
+type bird_species = Pigeon | Cardinal | Owl | BlueJay | KingFisher
 
 (** The type of bird that contains the following fields: species, size, hits *)
 type bird = {species : bird_specie; size : int; hits : int}
