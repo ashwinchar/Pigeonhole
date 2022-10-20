ccd4522a0ab740e513fa3c33cda7bc61bd8a14ff
arjuuunhm <96384102+arjuuunhm@users.noreply.github.com>
2022-10-20 15:35:21 -0400

    bug error
    resolved unnamed variable

--- src/pigeonhole.ml
+++ src/pigeonhole.ml
@@ -2,7 +2,7 @@
 type bird_species = Pigeon | Cardinal | Owl | BlueJay | KingFisher
 
 (** The type of bird that contains the following fields: species, size, hits *)
-type bird = {species : bird_specie; size : int; hits : int}
+type bird = {species : bird_species; size : int; hits : int}
 
 (** Type to identify tiles on the game board*)
 type coordinate = char * int
