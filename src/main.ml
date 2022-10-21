4c506778fc45198804b0f2a0f6303237dd89d9f8
arjuuunhm <96384102+arjuuunhm@users.noreply.github.com>
2022-10-21 13:34:59 -0400

    Update main.ml
    Added a play and quit option for user

--- src/main.ml
+++ src/main.ml
@@ -1,17 +1,22 @@
 open Graphics
+let create_grid () = 
+  clear_graph()
 
-(*let rec interactive () = let event = wait_next_event [ Key_pressed ] in if
-  event.key == 'q' then exit 0 else print_char event.key; print_newline ();
-  interactive ()*)
+let rec interactive () = let event = wait_next_event [ Key_pressed ] in if
+  event.key == 'q' then exit 0 else if event.key == 'p' then create_grid () else print_char event.key; print_newline ();
+  interactive ()
 
-let rec loop () = loop ()
+(* let rec loop () = loop () *)
 
 let init () =
   open_graph "";
   set_window_title "Pigeon Hole";
   draw_rect ((size_x () / 2) - 150) ((size_y () / 2) - 100) 300 200;
-  set_color red;
+  set_color blue;
   fill_rect ((size_x () / 2) - 150) ((size_y () / 2) - 100) 300 200;
-  loop ()
+  set_color black;
+  moveto (size_x()/2 - 75) ((size_y () - 100));
+  draw_string "Press P to Play, Q to Quit";
+  interactive ()
 
 let () = init ()
