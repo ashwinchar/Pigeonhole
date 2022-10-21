0cbec15062f2c9de4407b16c1515874e4c98523a
abhigoyal29 <83799253+abhigoyal29@users.noreply.github.com>
2022-10-20 21:00:20 -0400

    Added install.txt and started working with game library

--- src/pigeonhole.ml
+++ src/pigeonhole.ml
@@ -1,8 +1,16 @@
-(** The type of bird that occupy the board *)
-type bird_species = Pigeon | Cardinal | Owl | BlueJay | KingFisher
+type bird_species =
+  | Pigeon
+  | Cardinal
+  | Owl
+  | BlueJay
+  | KingFisher  (** The type of bird that the player can "shoot" into a hole *)
 
+type bird = {
+  species : bird_species;
+  size : int;
+  hits : int;
+}
 (** The type of bird that contains the following fields: species, size, hits *)
-type bird = {species : bird_species; size : int; hits : int}
 
-(** Type to identify tiles on the game board*)
 type coordinate = char * int
+(** Type to identify tiles on the game board *)
