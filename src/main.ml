6959bad860677c8e5cb9ccb63f0daadabde9f7f2
arjuuunhm <96384102+arjuuunhm@users.noreply.github.com>
2022-10-21 20:31:21 -0400

    implemented horizontal lines
    Created horizontal lines for the creation of our game grid

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,11 @@
 open Graphics
-
+let rec horizontal_lines (count : int) (x : int) (y : int) (offset : int) = 
+  if count = 0 then () 
+  else (
+    moveto x y;
+    lineto (size_x () - offset) y;
+    horizontal_lines (count - 1) x (y + 50) offset
+  )
 let rec vertical_lines (count : int) (x : int) (y : int) (offset : int) =
   if count = 0 then ()
   else (
@@ -9,7 +15,8 @@ let rec vertical_lines (count : int) (x : int) (y : int) (offset : int) =
 
 let create_grid (x : int) (y : int) (offset : int) =
   clear_graph ();
-  vertical_lines 10 x y offset
+  vertical_lines 10 x y offset;
+  horizontal_lines 10 x y offset
 
 let rec interactive () =
   let event = wait_next_event [ Key_pressed ] in
@@ -19,11 +26,10 @@ let rec interactive () =
   print_newline ();
   interactive ()
 
-(* let rec loop () = loop () *)
-
 let init () =
   open_graph "";
   set_window_title "Pigeon Hole";
+  resize_window 1200 720;
   draw_rect ((size_x () / 2) - 90) ((size_y () / 2) - 40) 180 80;
   set_color green;
   fill_rect ((size_x () / 2) - 90) ((size_y () / 2) - 40) 180 80;
