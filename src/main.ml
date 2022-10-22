7403eeae22064f7e69debbd15b72a0227fa175ea
ashwinchar <charashwin@gmail.com>
2022-10-21 20:08:54 -0400

    Implement vertical_lines
    -Create vertical lines helper function for create_grid

--- src/main.ml
+++ src/main.ml
@@ -1,9 +1,22 @@
 open Graphics
-let create_grid () = 
-  clear_graph()
 
-let rec interactive () = let event = wait_next_event [ Key_pressed ] in if
-  event.key == 'q' then exit 0 else if event.key == 'p' then create_grid () else print_char event.key; print_newline ();
+let rec vertical_lines (count : int) (x : int) (y : int) (offset : int) =
+  if count = 0 then ()
+  else (
+    moveto x y;
+    lineto x (size_y () - offset);
+    vertical_lines (count - 1) (x + 50) y offset)
+
+let create_grid (x : int) (y : int) (offset : int) =
+  clear_graph ();
+  vertical_lines 10 x y offset
+
+let rec interactive () =
+  let event = wait_next_event [ Key_pressed ] in
+  if event.key == 'q' then exit 0
+  else if event.key == 'p' then create_grid 0 0 20
+  else print_char event.key;
+  print_newline ();
   interactive ()
 
 (* let rec loop () = loop () *)
@@ -11,11 +24,11 @@ let rec interactive () = let event = wait_next_event [ Key_pressed ] in if
 let init () =
   open_graph "";
   set_window_title "Pigeon Hole";
-  draw_rect ((size_x () / 2) - 150) ((size_y () / 2) - 100) 300 200;
-  set_color blue;
-  fill_rect ((size_x () / 2) - 150) ((size_y () / 2) - 100) 300 200;
+  draw_rect ((size_x () / 2) - 90) ((size_y () / 2) - 40) 180 80;
+  set_color green;
+  fill_rect ((size_x () / 2) - 90) ((size_y () / 2) - 40) 180 80;
   set_color black;
-  moveto (size_x()/2 - 75) ((size_y () - 100));
+  moveto ((size_x () / 2) - 75) ((size_y () / 2) - 5);
   draw_string "Press P to Play, Q to Quit";
   interactive ()
 
