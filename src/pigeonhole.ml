7e010aa04fcb5e1a954c9897732196f765a0e601
Arjun Mehta <96384102+arjuuunhm@users.noreply.github.com>
2022-11-25 22:57:24 -0500

    implemented textbase types
    description

--- src/pigeonhole.ml
+++ src/pigeonhole.ml
@@ -1,3 +1,4 @@
+
 type bird_species =
   | Pigeon
   | Cardinal
@@ -7,26 +8,27 @@ type bird_species =
 
 type bird = {
   species : bird_species;
-  size : int;
-  hits : int;
+  points : int
 }
 (** The type of bird that contains the following fields: species, size, hits *)
 
-type coordinate = char * int
+type grid_square = {
+  coordinate : char * int;
+  mutable occupied : bool;
+  mutable shot_at : bool;
+}
 (** Type to identify tiles on the game board *)
 
 type grid_player = {
-  point : coordinate;
-  occupied : bool;
-  hit : bool;
-  shot_at : bool;
+  grid : grid_square array;
+  mutable score : int
 }
 
 type grid_opponent = {
-  point : coordinate;
-  shot_at : bool;
-  hit : bool;
+  grid : grid_square array;
+  mutable score : int
 }
 
-let rows = [ 'a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j' ]
-let columns = [ 0; 1; 2; 3; 4; 5; 6; 7; 8; 9 ]
+let rows = [| 'a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j' |]
+let columns = [| 0; 1; 2; 3; 4; 5; 6; 7; 8; 9 |]
+
